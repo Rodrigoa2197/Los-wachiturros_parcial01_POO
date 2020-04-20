a063047a132baedb5dc49d1dafa12c133be19f08
Rodrigoa2197 <54407311+Rodrigoa2197@users.noreply.github.com>
2020-04-19 23:55:18 -0600

    clase empresa

--- src/com/RAN/x00303318/Empresa.java
+++ src/com/RAN/x00303318/Empresa.java
@@ -3,52 +3,61 @@ package com.RAN.x00303318;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Empresa extends Empleado {
-    //Atributos
+public class Empresa {
     private String nombre;
-    private List<Empleado> planilla;
+    private List<Empleado> planilla = new ArrayList<Empleado>();
+
     //Constructor
-
-
-    public Empresa(String nombre, String numero, String nombre1, String puesto, double salario, String nombre2) {
-        super(nombre, numero, nombre1, puesto, salario);
-        this.nombre = nombre2;
+    public Empresa(String nombre) {
+        this.nombre = nombre;
     }
 
     //getters y setters
     public String getNombre() {
         return nombre;
     }
+
     public List<Empleado> getPlanilla() {
-        return planilla;
-    }
-    /* public void addEmpleado(List<Empleado>empleados){
-        empleados.add(Empleado);
-    }*/ /*COMPLETAR CLASE EMPLEADOS PARA SEGUIR*/
-}
-//ORIGINAL, COMO SE PIDE EN EL UML
-/*public class Empresa{
-    private String nombre;
-    private List<Empleado> planilla = new ArrayList<Empleado>();
-
-    //Constructor
-    public Empresa(String nombre){
-        this.nombre = nombre;
-    }
-
-    //Setters & Getters
-    public String getNombre(){
-        return nombre;
-    }
-    public List<Empleado> getPlanilla(){
         return planilla;
     }
 
     //Metodos
-    public void addEmpleado(Empleado empleado){
-
+    public void addEmpleado(Empleado empleado) {
+        planilla.add(empleado);
     }
-    public void quitEmpleado(String nombre){
 
+    public void quitEmpleado(String nombre) {
+        boolean quit = false;
+        int cont = 0;
+        char confirma;
+        for (Empleado chale : planilla) {
+            if (chale.getNombre().equalsIgnoreCase(nombre)) {
+                String datos = null;
+                for (Documento a : chale.getDocumentos()) {
+                    datos += a.getNombre() + ": " + a.getNumero() + "\n";
+                }
+                if (chale instanceof PlazaFija) {
+                    confirma = JOptionPane.showInputDialog(null, "Empleado a despedir" + "\n" + "Nombre: " + chale.getNombre() + "\n" + "Puesto: " + chale.getPuesto() + "\n" + datos + "Salario: $" + chale.getSalario() + "\n" + "Extensión: " + ((PlazaFija) chale).getExtensión()).charAt(0);
+
+                        if (confirma == 'Y') {
+                            quit = true;
+                        } else if (confirma == 'y') {
+                            quit = true;
+                        }else{}
+                } else if (chale instanceof ServicioProfesional) {
+                    confirma = JOptionPane.showInputDialog(null, "Empleado a despedir\"" + "\n" + "Nombre: " + chale.getNombre() + "\n" + "Puesto: $" + chale.getPuesto() + "\n" + datos + "Salario: " + chale.getSalario() + "\n" + "Meses: " + ((ServicioProfesional) chale).getMeses()).charAt(0);
+                    if (confirma == 'Y') {
+                        quit = true;
+                    } else if (confirma == 'y') {
+                        quit = true;
+                    }else{}
+                }
+            }
+            cont++;
+        }
+        if(quit == true){
+            planilla.remove(cont);
+            JOptionPane.showMessageDialog(null,"¡Se guardaron los cambios exitosamente!");
+        }
     }
-}*/
+}
