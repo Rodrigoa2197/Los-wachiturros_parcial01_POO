8d44c5dd1e370daf0860136bf4d6e463b16065fa
Rodrigoa2197 <54407311+Rodrigoa2197@users.noreply.github.com>
2020-04-19 23:29:15 -0600

    se modifico la clase empleado

--- src/com/RAN/x00303318/Empleado.java
+++ src/com/RAN/x00303318/Empleado.java
@@ -2,22 +2,18 @@ package com.RAN.x00303318;
 
 import java.awt.*;
 import java.util.List;
-
-public abstract class Empleado extends Documento{
+public abstract class Empleado{
     //Atributos
     protected String nombre;
     protected String puesto;
     protected List<Documento> documentos;
     protected double salario;
     //Constructor
-
-    public Empleado(String nombre, String numero, String nombre1, String puesto, double salario) {
-        super(nombre, numero);
-        this.nombre = nombre1;
+    public Empleado(String nombre, String puesto, double salario) {
+        this.nombre = nombre;
         this.puesto = puesto;
         this.salario = salario;
     }
-
     //Getters y setters
     public String getNombre() {
         return nombre;
@@ -35,44 +31,13 @@ public abstract class Empleado extends Documento{
         this.salario = salario;
     }
     //metodos
+    //adicionar
+    public void addDocumento(Documento a){
+        documentos.add(a);
+    }
+    //remover
+    public void removeDocumento(String a){
+        documentos.removeIf(s -> (s.getNombre().equals(a)));
+    }
 
 }
-//ORIGINAL, COMO SE PIDE EN EL UML
-/*abstract class Empleado {
-    protected String nombre = "\0";
-    protected String puesto = "\0";
-    protected List<Documento> documentos = new ArrayList<Documento>();
-    protected double salario = 0.00f;
-
-    public Empleado(String nombre, String puesto, double salario) {
-        this.nombre = nombre;
-        this.puesto = puesto;
-        this.salario = salario;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public String getPuesto() {
-        return puesto;
-    }
-
-    public List<Documento> getDocumentos() {
-        return documentos;
-    }
-
-    public void addDocumento(Documento a){
-    }
-
-    public void removeDocumento(String a){
-    }
-
-    public double getSalario() {
-        return salario;
-    }
-
-    public void setSalario(double salario) {
-        this.salario = salario;
-    }
-}*/
